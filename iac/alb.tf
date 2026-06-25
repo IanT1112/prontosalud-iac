b8a871a6fb576bad5f68349d5972ad2d60c657b5
IanT1112 <iantapia1104@gmail.com>
2026-06-24 23:25:37 -0500

    fix: corrección configuración de seguridad del alb

--- iac/alb.tf
+++ iac/alb.tf
@@ -43,17 +43,17 @@ resource "aws_lb_target_group" "backend" {
 }
 
 resource "aws_lb_listener" "http" {
-
   load_balancer_arn = aws_lb.main.arn
-
-  port = 80
-
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
 
-    type = "forward"
-
-    target_group_arn = aws_lb_target_group.backend.arn
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
