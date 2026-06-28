b4a021ac933466a29ce9bece43df165a8e3f0211
IanT1112 <iantapia1104@gmail.com>
2026-06-27 21:48:41 -0500

    fix: se habilitan logs de auditoría en DocumentDB- ERROR:CKV_AWS_104

--- iac/documentdb.tf
+++ iac/documentdb.tf
@@ -15,6 +15,11 @@ resource "aws_docdb_cluster_parameter_group" "main" {
     name  = "tls"
     value = "disabled"
   }
+
+  parameter {
+    name  = "audit_logs"
+    value = "enabled"
+  }
 }
 
 resource "aws_docdb_cluster" "main" {
