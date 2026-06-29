fdb06469c593d35f2543a2c189fd3ba3c1cad600
IanT1112 <iantapia1104@gmail.com>
2026-06-29 02:16:52 -0500

    fix: se habilita container insights en el clúster ECS-ERROR:CKV_AWS_65

--- iac/ecs.tf
+++ iac/ecs.tf
@@ -1,5 +1,10 @@
 resource "aws_ecs_cluster" "main" {
   name = "prontosalud-cluster"
+
+    setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
 }
 
 resource "aws_ecs_task_definition" "backend" {
