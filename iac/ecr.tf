2b76c2b9cd7e19c0f97d46c925e6ead822e4ba3d
IanT1112 <iantapia1104@gmail.com>
2026-06-29 02:05:57 -0500

    fix: se habilita la inmutabilidad de etiquetas en ECR-ERROR:CKV_AWS_51

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "backend" {
 
   name = "prontosalud-backend"
 
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
