812a8f4cc31f7c2c182c4da1df9d14c703e32bad
IanT1112 <iantapia1104@gmail.com>
2026-06-29 02:27:51 -0500

    fix: se deshabilita la asignación automática de ip pública en subredes-ERROR:CKV_AWS_130

--- iac/networking.tf
+++ iac/networking.tf
@@ -31,7 +31,7 @@ resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "public-a"
@@ -43,7 +43,7 @@ resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "public-b"
