16ff7a2004e1e92e3501c7714d4c3dffd665492b
IanT1112 <iantapia1104@gmail.com>
2026-06-30 01:49:57 -0500

    fix: se agregan descripciones al security group de ECS y sus reglas-ERROR:CKV_AWS_23

--- iac/security_groups.tf
+++ iac/security_groups.tf
@@ -38,18 +38,16 @@ resource "aws_security_group" "alb_sg" {
 ##################################################
 
 resource "aws_security_group" "ecs_sg" {
-
-  name = "ecs-sg"
-
-  vpc_id = aws_vpc.main.id
+  name        = "ecs-sg"
+  description = "Security group for ECS tasks"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
+    description = "Allow traffic from ALB to ECS application"
 
     from_port = 3000
-
-    to_port = 3000
-
-    protocol = "tcp"
+    to_port   = 3000
+    protocol  = "tcp"
 
     security_groups = [
       aws_security_group.alb_sg.id
@@ -57,13 +55,11 @@ resource "aws_security_group" "ecs_sg" {
   }
 
   egress {
+    description = "Allow outbound traffic from ECS tasks"
 
-    from_port = 0
-
-    to_port = 0
-
-    protocol = "-1"
-
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
